a18b673592b9f9f2ce1514c7679eff2f28ad5bbb
Maaz Saboowala <143613269+MaazS07@users.noreply.github.com>
2024-09-28 14:42:20 +0530

    Update todo-controller.js

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -1,19 +1,23 @@
 const Todo = require('../model/Todo');
 
-// Get all todos
+// Get all todos for a specific user
 exports.getTodos = async (req, res) => {
   try {
-    const todos = await Todo.find().sort({ createdAt: -1 });
+    const userId = req.params.userId;
+    const todos = await Todo.find({ userId }).sort({ createdAt: -1 });
     res.json(todos);
   } catch (err) {
     res.status(500).json({ message: 'Error fetching todos' });
   }
 };
 
-// Create a new todo
+// Create a new todo for a specific user
 exports.createTodo = async (req, res) => {
   try {
-    const newTodo = new Todo(req.body);
+    const newTodo = new Todo({
+      ...req.body,
+      userId: req.params.userId
+    });
     const savedTodo = await newTodo.save();
     res.status(201).json(savedTodo);
   } catch (err) {
@@ -21,12 +25,16 @@ exports.createTodo = async (req, res) => {
   }
 };
 
-// Update a todo
+// Update a todo (ensure it belongs to the user)
 exports.updateTodo = async (req, res) => {
   try {
-    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const updatedTodo = await Todo.findOneAndUpdate(
+      { _id: req.params.id, userId: req.params.userId },
+      req.body,
+      { new: true }
+    );
     if (!updatedTodo) {
-      return res.status(404).json({ message: 'Todo not found' });
+      return res.status(404).json({ message: 'Todo not found or not authorized' });
     }
     res.json(updatedTodo);
   } catch (err) {
@@ -34,12 +42,12 @@ exports.updateTodo = async (req, res) => {
   }
 };
 
-// Delete a todo
+
 exports.deleteTodo = async (req, res) => {
   try {
-    const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
+    const deletedTodo = await Todo.findOneAndDelete({ _id: req.params.id, userId: req.params.userId });
     if (!deletedTodo) {
-      return res.status(404).json({ message: 'Todo not found' });
+      return res.status(404).json({ message: 'Todo not found or not authorized' });
     }
     res.json({ message: 'Todo deleted successfully' });
   } catch (err) {
